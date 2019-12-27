0b740a67ed985591bb097de1122dc1c58f1de695
SofianSaleh <sofiane.saleh11@gmail.com>
2019-12-27 12:21:53 +0100

    mad it alternate between X and O needs refactoring

--- challenge_1/app.js
+++ challenge_1/app.js
@@ -1,2 +1,12 @@
-console.log('hi')
+var count = 0;
+var getId = (id) => {
+    var x = document.getElementsByClassName('tile');
+    if( count % 2 === 0 ){
+        x[id].innerHTML = "X";
+        count++;
+    }else{
+        x[id].innerHTML = "O";
+        count++;
+    }
+}
 
