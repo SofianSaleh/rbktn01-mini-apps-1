277a75cd5649b86ab44b0d5d9784d92564888f92
SofianSaleh <sofiane.saleh11@gmail.com>
2019-12-30 14:16:06 +0100

    made the basic file structures

--- challenge_3/client/app.jsx
+++ challenge_3/client/app.jsx
@@ -1,2 +1,51 @@
+class App extends React.Component {
+    constructor() {
+        super()
+        this.state = {
+            Checkout: true,
+            FormOne: false,
+            FormTwo: false,
+            FormThree: false
+        }
+    }
 
+    handleCheckOut() {
+        this.setState({
+          checkout: false,
+          formOne: true
+        });
+      }
+    handleFormOne() {
+        this.setState({
+            FormOne: false,
+            FormTwo: true
+        });
+    }
+    handleFormTwo() {
+        this.setState({
+            FormTwo: false,
+            FormThree: true
+        });
+    }
+
+    handleSubmit() {
+        this.setState({
+            FormThree: false,
+            Submit: true
+        });
+
+    }
+    render() {
+        return (
+            <div>
+
+                <button onClick={this.handleCheckout}>checkout</button>
+                <script>{console.log(this.state.Checkout)}</script>
+            </div>
+        )
+    }
+}
+
+ReactDOM.render(<App />,
+    document.getElementById('root'))
 
--- challenge_3/server.js
+++ challenge_3/server.js
@@ -1,2 +1,23 @@
+const express = require('express')
+const bodyParser = require('body-parser')
+const port = 1000;
+var app = express()
+ 
+app.listen(port, () => {
+    console.log("I'm listening at 1000")
+})
+var jsonParser = bodyParser.json()
+app.use(express.static('public'))
+app.use(bodyParser.json())
+var urlencodedParser = bodyParser.urlencoded({ extended: false })
+ 
 
+// app.get('/', urlencodedParser, function (req, res) {
+  
+// })
+ 
+//  bodies
+// app.post('/api/users', jsonParser, function (req, res) {
+  
+// })
 
