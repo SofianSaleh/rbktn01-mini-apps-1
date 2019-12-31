0e6abe744a1d2835953b05950bc0fe0f048bd803
SofianSaleh <sofiane.saleh11@gmail.com>
2019-12-31 09:51:05 +0100

    added to the database

--- challenge_3/config/database.js
+++ challenge_3/config/database.js
@@ -1,11 +1,40 @@
 var mongoose = require("mongoose");
 
 mongoose.connect(
-  "mongodb://localhost/checkout",
-  { useNewUrlParser: true }
+  "mongodb://localhost:27017/checkout",
+  { useNewUrlParser: true, useUnifiedTopology: true }
 );
 var db = mongoose.connection;
 
 db.once("open", () =>
   console.log(`Connected to MongoDB on ${db.host}:${db.port}`)
 );
+
+
+var User = new mongoose.Schema({
+  Name:  String,
+  Email:  String,
+  Password:  String,
+  street:  String,
+  city:  String,
+  state:  String,
+  zipCode:  String,
+  phone: String,
+  cardNumber:  Number,
+  expiration:  Date,
+  cvv:  Number,
+  Billing_address:  String
+  // Name: String,
+  // Email: String,
+  // Password: Number,
+  // Line1: String,
+  // City: String,
+  // State: String,
+  // CreditCard: Number,
+  // ExpireDate: Date,
+  // Cvv: Number,
+  // BillingZip: Number
+});
+
+var user = mongoose.model('User', User, 'checkout');
+module.exports = user; 
--- challenge_3/server.js
+++ challenge_3/server.js
@@ -1,8 +1,8 @@
 const express = require('express')
 const bodyParser = require('body-parser')
-const userRouter = require('./models/userSchema')
+// const userRouter = require('./models/userSchema')
 const port = process.env.PORT || 1000;
-require("./config/database");
+const User = require('./config/database.js')
 var app = express()
  
 app.listen(port, () => {
@@ -12,9 +12,14 @@ app.listen(port, () => {
 app.use(express.static('public'))
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: false }))
-app.use('/userSchema', userRouter)
+// app.use('/userSchema', userRouter)
 app.post('/user', (req, res) => {
     console.log(req.body)
+    // var data = JSON.parse(req.body)
+    User.create(req.body,(err, res) =>{
+        if(err)return console.log('err')
+        console.log("success")
+    })
 })
 
 
